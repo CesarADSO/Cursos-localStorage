64d1a0641b7ce6d0cf14fc7f2e5480ddb8af9b60
Cesar Morales <191783025+CesarADSO@users.noreply.github.com>
2025-09-25 13:59:20 -0500

    Practiqué el como utilizar el local storage en nuestros proyectos

--- main.js
+++ main.js
@@ -1,39 +1,51 @@
 const form = document.getElementById('formulario');
-const input = document.getElementById('nombre');
-const saludo = document.getElementById('saludo');
+const inputCurso = document.getElementById('curso');
+const inputProfesor = document.getElementById('profesor');
+const inputPrecio = document.getElementById('precio');
+const inputCiudad = document.getElementById('ciudad');
+const inputCupo = document.getElementById('cupo');
+const mensaje = document.getElementById('mensaje-curso');
 const btnEliminar = document.getElementById('borrar');
-
-
-
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const nombre = input.value.trim();
+    const curso = inputCurso.value.trim();
+    const profesor = inputProfesor.value.trim();
+    const precio = inputPrecio.value.trim();
+    const ciudad = inputCiudad.value.trim();
+    const cupo = inputCupo.value.trim();
 
-    if (nombre == "") {
-        alert("Por favor ingresa un nombre");
-        return;
+    if (curso == "" || profesor == "" || precio == "" || ciudad == "" || cupo == "") {
+        alert("Por favor rellene los campos, ninguno puede estar vacio, gracias.");
     }
 
-    localStorage.setItem('usuario', nombre);
-    saludo.textContent = 'bienvenido ' + nombre;
+    // EN EL EJERCICIO ANTERIOR ERA UN SOLO VALOR, AHORA ESTAMOS ALIMENTANDO UN OBJETO CON SUS DIFERENTES CLAVES Y VALORES
+    const nuevoCurso = {
+        nombre: curso,
+        profesor: profesor,
+        precio: precio,
+        ciudad: ciudad,
+        cupo: cupo
+    };
 
-    input.value = "";
+    localStorage.setItem('curso', JSON.stringify(nuevoCurso))
 
-
+    form.reset();
+    
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    const usuarioGuardado = localStorage.getItem('usuario');
-    if (usuarioGuardado) {
-        saludo.textContent = 'bienvenido de nuevo ' + usuarioGuardado;
+    const cursoCreado = localStorage.getItem('curso');
+    if (cursoCreado) {
+        // JSON.parse se utliza para convertir un objeto que estaba convertido a texto para convertirlo a objeto de nuevo
+        const objetoCurso = JSON.parse(cursoCreado);
+        mensaje.innerHTML = ' Curso: ' + objetoCurso.nombre + '<br>'  + ' Profesor: ' + objetoCurso.profesor + '<br>' + ' Precio: ' + objetoCurso.precio + '<br>' + ' Ciudad: ' + objetoCurso.ciudad + '<br>' + ' Cupo: ' + objetoCurso.cupo;
     }
 });
 
-btnEliminar.addEventListener('click', () => {
-    localStorage.removeItem('usuario');
-    saludo.textContent = 'Bienvenido usuario indefinido';
-});
+
+
+
+
 
--- perfil.js
+++
@@ -1,7 +0,0 @@
-const crearUsuario = document.getElementById('usuario-creado');
-document.addEventListener('DOMContentLoaded', () => {
-    const usuarioGuardado = localStorage.getItem('usuario');
-    if (usuarioGuardado) {
-        crearUsuario.textContent = 'bienvenido de nuevo ' + usuarioGuardado;
-    }
-})
